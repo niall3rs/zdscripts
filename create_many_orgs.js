1a0d00531c9de1c0a4e4fd0ce71dfb3df841bf80
Niall Colfer <niall@colfer.me>
2016-05-21 18:14:49 +0100

    updated create_many_orgs

--- create_many_orgs.js
+++ create_many_orgs.js
@@ -1,11 +1,17 @@
-var num = 1000;
+var num = 4000;
+
 for (var i=0; i<Math.ceil(num/100); i++) {
 	var data = '{"organizations": [';
-	for (var j=0; j<Math.min(100,num); j++) {
-		data += '{"name":"BulkOrg' + ((100*i) + (j+1)) + '"},'
+	for (var j=i*100; j<Math.min((i+1)*100,num); j++) {
+		data += '{"name":"BulkOrg' + (j+1) + '"},'
 	}
-	date.slice(0,-1);
-	console.log(data);
+	data = data.slice(0,-1);
+	data = data + ']}';
+	(function(index) {
+		$.ajax("/api/v2/organizations/create_many.json", {
+			"type": "POST",
+			"contentType": "application/JSON",
+			"data": data
+		})
+	})(data);
 }
-
-working on it
